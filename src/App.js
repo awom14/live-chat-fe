11cd64d6d63305763ddc1ff50b424196715b8bb8
delfaaan <delfan@ilcs.co.id>
2023-02-06 09:36:34 +0700

    modified a little.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Register from './pages/Register';
 import './App.css';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/home" element={<Home/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/home" element={<Home/>} />
+			<Route path="/register" element={<Register/>} />
+			<Route path="/login" element={<Login/>} />
+		</Routes>
+	);
 }
 
 export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,47 +2,47 @@ import React from 'react'
 import { Form, Input, Label, Button, Row, Col } from 'reactstrap'
 
 const Login = () => {
-    const getToken = localStorage.getItem('token');
+	const getToken = localStorage.getItem('token');
 
-    const directToRegister = () => {
-        window.location.href = '/register';
-    };
+	const directToRegister = () => {
+		window.location.href = '/register';
+	};
 
-    return (
-        <>
-            <Row className='my-5'>
-                <Col className='d-flex justify-content-center align-items-center fs-5' style={{ fontWeight: 'bold' }}>Login Page</Col>
-            </Row>
-            <Row>
-                <div className='d-flex justify-content-center align-items-center'>
-                    <Form className='border w-50'>
-                        <Row className='pt-5 px-5 pb-3 mx-auto'>
-                            <Col md={3}>
-                                <Label>Email</Label>
-                            </Col>
-                            <Col md={9}>
-                                <Input type='email' placeholder='Enter email' />
-                            </Col>
-                        </Row>
-                        <Row className='px-5 pb-3 mx-auto'>
-                            <Col md={3}>
-                                <Label>Password</Label>
-                            </Col>
-                            <Col md={9}>
-                                <Input type='password' placeholder='Password' />
-                            </Col>
-                        </Row>
-                        <div className='d-flex justify-content-center align-items-center'>
-                            <Button className='mb-5'>Login</Button>
-                        </div>
-                        <div className='d-flex justify-content-center align-items-center'>
-                            <Label>No Account Yet? Create <span onClick={directToRegister} className='hvrPointer' style={{ cursor: 'pointer' }}>here!</span></Label>
-                        </div>
-                    </Form>
-                </div>
-            </Row>
-        </>
-    )
+	return (
+		<>
+			<Row className='my-5'>
+				<Col className='d-flex justify-content-center align-items-center fs-5' style={{ fontWeight: 'bold' }}>Login Page</Col>
+			</Row>
+			<Row>
+				<div className='d-flex justify-content-center align-items-center'>
+					<Form className='border w-50'>
+						<Row className='pt-5 px-5 pb-3 mx-auto'>
+							<Col md={3}>
+								<Label>Email</Label>
+							</Col>
+							<Col md={9}>
+								<Input type='email' placeholder='Enter email' />
+							</Col>
+						</Row>
+						<Row className='px-5 pb-3 mx-auto'>
+							<Col md={3}>
+								<Label>Password</Label>
+							</Col>
+							<Col md={9}>
+								<Input type='password' placeholder='Password' />
+							</Col>
+						</Row>
+						<div className='d-flex justify-content-center align-items-center'>
+							<Button className='mb-5'>Login</Button>
+						</div>
+						<div className='d-flex justify-content-center align-items-center'>
+							<Label>No Account Yet? Create <span onClick={directToRegister} className='hvrPointer' style={{ cursor: 'pointer' }}>here!</span></Label>
+						</div>
+					</Form>
+				</div>
+			</Row>
+		</>
+	)
 }
 
-export default Login
+export default Login;
