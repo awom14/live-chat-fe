0c6ad045f7250e6005f846e9e9bc8afd13da8d87
delfaaan <delfan@ilcs.co.id>
2023-02-06 17:23:25 +0700

    add register logic.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 		<Routes>
 			<Route path="/home" element={<Home/>} />
 			<Route path="/register" element={<Register/>} />
-			<Route path="/login" element={<Login/>} />
+			<Route path="/" element={<Login/>} />
 		</Routes>
 	);
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-import { Form, Input, Label, Button, Row, Col } from 'reactstrap'
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Label, Button, Row, Col, FormFeedback } from 'reactstrap';
+import axios from 'axios';
 
 const Register = () => {
 	const directToLogin = () => {
-		window.location.href = '/login';
+		window.location.href = '/';
+	};
+
+	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [rePassword, setRePassword] = useState('');
+	const [isValid, setIsValid] = useState(false);
+
+	const data = { username, email, password, rePassword };
+
+	useEffect(() => {
+		if (password !== rePassword) {
+			setIsValid(true);
+		} else {
+			setIsValid(false);
+		}
+		console.log(isValid);
+	}, [rePassword])
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		console.log(data);
+		try {
+			const result = await axios.post('http://localhost:1337/register', data);
+			console.log(result);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
+
+	const getUsers = async () => {
+		const headers = {
+			'Authorization': 'yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkyN2VlMzM4LTYwZmMtNDhkMC04ZDQ0LTM1ZDBlNTEyNzU1ZCIsInVzZXJuYW1lIjoibW93YSIsImVtYWlsIjoibW93YUBnbWFpbC5jb20iLCJpYXQiOjE2NzU2NTUxMjEsImV4cCI6MTY3NTY1ODcyMX0.ps3NArkYY4KRbQwaFebAkf9v5cZiOEGLqlqepZmkgrI'}
+
+		try {
+			const res = await axios.get('http://localhost:1337/getUsers', {headers})
+			console.log(res.data.data);
+		} catch (err) {
+			console.log(err.message);
+		}
 	};
 
 	return (
@@ -13,13 +55,21 @@ const Register = () => {
 		</Row>
 		<Row>
 			<div className='d-flex justify-content-center align-items-center'>
-				<Form className='border w-50'>
+				<Form onSubmit={handleSubmit} className='border w-50'>
 					<Row className='pt-5 px-5 pb-3 mx-auto'>
+						<Col md={3}>
+							<Label>Username</Label>
+						</Col>
+						<Col md={9}>
+							<Input type='text' value={username} onChange={(event) => setUsername(event.target.value)} required placeholder='Enter Username' />
+						</Col>
+					</Row>
+					<Row className='px-5 pb-3 mx-auto'>
 						<Col md={3}>
 							<Label>Email</Label>
 						</Col>
 						<Col md={9}>
-							<Input type='email' placeholder='Enter email' />
+							<Input type='email' value={email} onChange={(event) => setEmail(event.target.value)} required placeholder='Enter email' />
 						</Col>
 					</Row>
 					<Row className='px-5 pb-3 mx-auto'>
@@ -27,7 +77,7 @@ const Register = () => {
 							<Label>Password</Label>
 						</Col>
 						<Col md={9}>
-							<Input type='password' placeholder='Password' />
+							<Input type='password' value={password} onChange={(event) => setPassword(event.target.value)} required placeholder='Password' />
 						</Col>
 					</Row>
 					<Row className='px-5 pb-3 mx-auto'>
@@ -35,7 +85,8 @@ const Register = () => {
 							<Label>Re-enter Password</Label>
 						</Col>
 						<Col md={9}>
-							<Input type='password' placeholder='Re-enter Password' />
+							<Input type='password' value={rePassword} autoFocus={isValid} onChange={(event) => setRePassword(event.target.value)} required invalid={isValid} placeholder='Re-enter Password' />
+							<FormFeedback>Please input the correct password.</FormFeedback>
 						</Col>
 					</Row>
 					<div className='d-flex justify-content-center align-items-center'>
